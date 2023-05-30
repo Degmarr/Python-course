343374abf923310f512b074d1b8c71813642bb53
Degmarr <karina-rakisheva@mail.ru>
2023-05-30 21:49:54 +0600

    meow

--- lesson9/lesson9.3.py
+++ lesson9/lesson9.3.py
@@ -1,9 +1,16 @@
 def num():
-    with open('numbers.txt', 'r') as file:
-        a = file.read()
-        for i in range(len(a)):
-            if int(i) == i:
-                with open('integers.txt', 'w') as file2:
-                    b = file.write(i)
-                    print(b)
+    numbers = []
+    with open("numbers.txt", "r") as f:
+        data = f.read().split()
+        for i in data:
+            try:
+                numbers.append(int(i))
+            except ValueError:
+                continue
+
+    print(numbers)
+    for num in numbers:
+        with open("integers.txt","a") as f2:
+            f2.write(str(num) + "\n")        
+                    
 num()                    
